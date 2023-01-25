921a7fda01d77ac2ab8bda1291d0739a7c860123
Flo Slv <florent3969@hotmail.fr>
2023-01-25 11:03:08 +0100

    🟪 refactor: clean code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
-import './globals.css'
+import { ReactNode } from "react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import "@/styles/globals.css";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body>{children}</body>
     </html>
-  )
-}
+  );
+};
+
+export default RootLayout;
