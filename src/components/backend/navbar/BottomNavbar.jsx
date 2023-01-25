b49ea5ea597e952b4e77dbe30638b88f2146e2c2
Flo Slv <florent3969@hotmail.fr>
2023-01-25 21:42:32 +0100

    🟦 feat: bottomNavbar responsive

--- src/components/backend/navbar/BottomNavbar.jsx
+++ src/components/backend/navbar/BottomNavbar.jsx
@@ -1,14 +1,48 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { signOut } from "next-auth/react";
+import clsx from "clsx";
+
+import { LayoutGrid, LogOut, User } from "lucide-react";
+
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/Tabs";
 
 const BottomNavbar = () => {
+  const pathname = usePathname();
+  const defaultValue = pathname.substring(1, pathname.length);
+
   return (
-    <Tabs defaultValue="account" className="w-[400px]">
-      <TabsList>
-        <TabsTrigger value="Dashboard">Dashboard</TabsTrigger>
-        <TabsTrigger value="Profile">Profile</TabsTrigger>
-        <TabsTrigger value="Logout">Sign out</TabsTrigger>
-      </TabsList>
-    </Tabs>
+    <div
+      className={clsx(
+        "fixed bottom-0 w-full",
+        "sm:bottom-10 sm:flex sm: justify-center"
+      )}
+    >
+      <Tabs defaultValue={defaultValue}>
+        <TabsList className="flex justify-between">
+          <Link href="/dashboard">
+            <TabsTrigger value="dashboard">
+              <LayoutGrid className="mr-2 h-4 w-4" />
+              Dashboard
+            </TabsTrigger>
+          </Link>
+
+          <Link href="/profile">
+            <TabsTrigger value="profile">
+              <User className="mr-2 h-4 w-4" />
+              Profile
+            </TabsTrigger>
+          </Link>
+
+          <TabsTrigger value="logout" onClick={signOut}>
+            <LogOut className="mr-2 h-4 w-4" />
+            Sign out
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
+    </div>
   );
 };
 
