d9291e4fcf9f7b60163c9c43aab32f52905a07fa
Flo Slv <florent3969@hotmail.fr>
2023-01-25 14:37:41 +0100

    🟨 style: homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/Button";
+
 const Home = () => {
-  return <main>Home page</main>;
+  return (
+    <main>
+      <div>Home page</div>
+      <div>
+        <Button variant="default">
+          <Link href="/api/auth/signin">Login</Link>
+        </Button>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
