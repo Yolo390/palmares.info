8936d55d6b48dffb531d080845b7646d7a2e051c
Flo Slv <florent3969@hotmail.fr>
2023-01-30 14:06:36 +0100

    🟪 refactor: getServerSession instead of unstable... for Signin

--- src/app/(frontend)/(auth)/signin/page.jsx
+++ src/app/(frontend)/(auth)/signin/page.jsx
@@ -1,15 +1,15 @@
 import { redirect } from "next/navigation";
-import { unstable_getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth/next";
 
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 import SignIn from "@/components/auth/SignIn";
 
 const Signin = async () => {
-  const session = await unstable_getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
   // If already connected, can not access to `/signin`.
-  if (session) redirect("/dashboard");
+  if (session) redirect("/admin/dashboard");
 
   return (
     <main className="flex flex-col justify-center items-center h-full">
