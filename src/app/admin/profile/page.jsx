7e2b99b2221b02c5d49ec1ba3a51c8419c744067
Flo Slv <florent3969@hotmail.fr>
2023-01-31 12:25:37 +0100

    🟦 feat: admin Page profile - fetch user w/ session.user.id

--- src/app/admin/profile/page.jsx
+++ src/app/admin/profile/page.jsx
@@ -1,5 +1,29 @@
-const Profile = () => {
-  return <div>Profile page</div>;
+import { getServerSession } from "next-auth/next";
+
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+import ProfileForm from "@/components/admin/form/Profile";
+
+const getUserById = async (id) => {
+  const res = await fetch(
+    `http://localhost:3000/api/admin/profile/getUserById?id=${id}`
+  );
+
+  const data = await res.json();
+
+  return data.user;
+};
+
+const Profile = async () => {
+  const session = await getServerSession(authOptions);
+
+  // To edit user session, there is no official way to do it w/ jwt...
+  // See: https://github.com/nextauthjs/next-auth/issues/2269
+  // See: https://github.com/nextauthjs/next-auth/issues/596
+  // That's why I fetch user from DB through API.
+  const user = await getUserById(session.user.id);
+
+  return <ProfileForm user={user} />;
 };
 
 export default Profile;
