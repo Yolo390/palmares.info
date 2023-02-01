163041c4e05082ec5b1a834d39a0c5b6bf6a02cc
Flo Slv <florent3969@hotmail.fr>
2023-02-01 14:34:46 +0100

    🟦 feat: prisma addSport

---
+++ src/lib/prisma/admin/sports.js
@@ -0,0 +1,25 @@
+import prisma from "@/lib/prisma/prismaClient";
+
+export const addSport = async (sport) => {
+  try {
+    const name = sport.name.trim();
+
+    // Check if name is not empty.
+    if (name === "") return { error: "Name can not be empty !" };
+
+    // Check if user already exist.
+    const existingSport = await prisma.sport.findUnique({
+      where: { name: name },
+    });
+
+    if (existingSport) return { error: "Sport already created !" };
+
+    const addedSport = await prisma.sport.create({
+      data: { name: name },
+    });
+
+    return { addedSport };
+  } catch (error) {
+    return { error };
+  }
+};
