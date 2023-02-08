98c92b3c1c5c154e76442bcf84f48192d29fc2d1
Flo Slv <florent3969@hotmail.fr>
2023-02-08 10:59:16 +0100

    🟪 refactor: remove useless code in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  // matcher: ["/dashboard", "/profile", "/addathlete", "/women", "/men"],
   matcher: ["/admin/:path*"],
 };
