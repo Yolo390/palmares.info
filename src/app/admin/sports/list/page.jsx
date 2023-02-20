3604f9f8f0275778add8588c752b265866d7999a
Flo Slv <florent3969@hotmail.fr>
2023-02-20 21:51:42 +0100

    🟦 feat: SportsList component

--- src/app/admin/sports/list/page.jsx
+++ src/app/admin/sports/list/page.jsx
@@ -1,5 +1,28 @@
-const SportsList = () => {
-  return <div>List of all sports</div>;
+import SportsList from "@/components/admin/list/SportsList";
+import clsx from "clsx";
+
+const Sports = async () => {
+  const res = await fetch("http://localhost:3000/api/admin/sport/getSports", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+  const { sports } = data;
+
+  return (
+    <div
+      className={clsx(
+        "flex flex-col items-center gap-5 mt-5 mb-24",
+        "sm:gap-8 sm:mt-12 sm:mb-32"
+      )}
+    >
+      <h1>List of all sports.</h1>
+
+      {sports &&
+        sports.map((sport) => <SportsList key={sport.id} sport={sport} />)}
+    </div>
+  );
 };
 
-export default SportsList;
+export default Sports;
